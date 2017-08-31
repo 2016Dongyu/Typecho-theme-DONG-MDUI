97bb857e3a427cbc7dcd7ea0a73db0398f26008a
2016dongyu <2016dongyu@gmail.com>
2017-08-31 15:40:44 +0800

    Signed-off-by: 2016dongyu <2016dongyu@gmail.com>

--- page-gallery.php
+++ page-gallery.php
@@ -6,42 +6,7 @@
 ?>
 <?php $this->need('header.php'); ?>
 
-<div class="mdui-container">
-	
-	
 
-	
-  <a class="mdui-color-theme mdui-ripple" mdui-dialog="{target: '#example-1'}"><img src="<?php $this->options->themeUrl(); ?>images/picHD_11.png" /></a>
-
-  <div class="mdui-dialog" id="example-1" style="">
-    	<img src="<?php $this->options->themeUrl(); ?>images/picHD_11.png" />
-    </div>
-   
-
-
-
-<Div class="main mdui-container m2" oncontextmenu="return false;">
-<div class="mdui-card mdui-typo mduijg mdui-hoverable">
-  <div class="mdui-card-media">
-    <img oncontextmenu="return false;" ondragstart="return false;" src="<?php echo showThumb($this,null,true); ?>"/>
-    <div class="mdui-card-media-covered mdui-card-media-covered-gradient">
-      <div class="mdui-card-primary">
-        <div class="mdui-card-primary-title">
-        	<span class="tit"><?php $this->title() ?></span>
-        	<time> © <?php $this->author(); ?>  <?php $this->date('Y-m-d'); ?></time></div>
-      </div>
-    </div>
-  </div>
-  <div class="mdui-card-actions mdui-typo" style="color: rgba(0,0,0,.8);padding: 20px;">
-  	<?php $this->content(); ?>
-  </div>
-
-</div>
-
-<div class="mdui-hoverable mdui-shadow-1" style="background-color: #ffffff;color: rgba(0,0,0,.8);padding: 20px;">
-<?php if($this->allow('comment')){$this->need('comments.php');}?>
-</div>
-</div>
 
 
 
---
+++ readme.md
@@ -0,0 +1,28 @@
+![logo.jpg](https://i.loli.net/2017/08/31/59a7b9f8b49df.jpg)
+![shezhi.png](https://i.loli.net/2017/08/31/59a7ba892a385.png)
+![shili.png](https://i.loli.net/2017/08/31/59a7ba8a4988c.png)
+# MDUI
+一个基于[mdui前端](https://github.com/zdhxiong/mdui)的typecho主题
+
+A typecho theme base on [mdui front end](https://github.com/zdhxiong/mdui)
+
+## General 概括
+
+[![Author](https://img.shields.io/badge/author-DONGYu-blue.svg?style=flat-square)](https://www.dongyu.site)
+![Version](https://img.shields.io/badge/version-1.0.0-green.svg?style=flat-square)
+[![typecho](https://img.shields.io/badge/typecho-1.0+-green.svg?style=flat-square)](http://typecho.org)
+
+
+## Contributing 贡献
+
+All kinds of contributions (enhancements, new features, documentation & code improvements, issues & bugs reporting) are welcome.
+
+欢迎各种形式的贡献，包括但不限于优化，添加功能，文档 & 代码的改进，问题和 bugs 的报告。期待您的 `Pull Request`。
+
+## License 许可证
+
+[![license](https://img.shields.io/github/license/2016dongyu/Typecho-theme-DONG-MDUI.svg?style=flat-square)](https://github.com/2016dongyu/Typecho-theme-DONG-MDUI/blob/master/LICENSE)
+
+Open sourced under the GPL v3.0 license.
+
+根据 GPL V3.0 许可证开源。
